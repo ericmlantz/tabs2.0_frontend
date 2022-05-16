e6ed913017b515e5bdb5c7650a95310cd472cbae
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-16 10:54:47 -0400

    last heroku update

--- src/pages/Tabs.js
+++ src/pages/Tabs.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Tabs = () => {
   return (
+    <div className="tabs-top">
     <div className='tabs-intro'>
       <p><span className='tabs-header'>Tabs 2.0</span> is the future of online research. This app allows you to take notes and save information, links, and knowledge that you can organize and then use however you like.
     </p>
     <p>Whether it be researching positions in a new job market or just figuring out what you are going to get your significent other for your anniversary, <span className='tabs-header'>Tabs 2.0</span> is the app for you!
     </p>
+    </div>
     </div>
   )
 }
