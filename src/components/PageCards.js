4ce4c9760bc2c9f84ec1f1ccbe565ee4f85eabb6
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-16 10:56:23 -0400

    update style

--- src/components/PageCards.js
+++ src/components/PageCards.js
@@ -3,7 +3,7 @@ const PageCards = ({page, pages, setPages, getInterestCard, interestcard, handle
   return (
     <div className='page-container'>
       <div className="note-title"><b>{page.title}</b><br/>
-      {page.url}<hr/>
+      {page.url}
       <div>
 
         <button className='delete-page' onClick={() => handleRemove(page.id)}>Delete Page</button>
